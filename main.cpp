edc5b72d82feb96ed60414c36d91ba9304be23e0
ACSoverride <mattinhvt@gmail.com>
2022-09-27 19:57:43 -0400

    started working on project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
-
+#include <cmath>
+#include <vector>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+	int input1;
+	int dividend;
+	bool looper;
+	vector<int> remain;
+	cout << "please enter a number to convert to hexadecimal";
+	cin >> input1;
+	dividend = input1;
+	while (looper){
+		remain.push_back(dividend%16);
+		dividend = dividend/16;
+		if (dividend/16 == 0){
+
+		}
+	}
+	return 0;
 }
